4cb33aee29e13b5462b61fc17cfe84079115ffc4
Babyd12 <mamadcir24@gmail.com>
2023-10-19 19:47:01 +0000

    Trello--> J'ai fini le HTML et le CSS de la page d'inscription et de connexion

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion de tâche</title>
+    <link rel="stylesheet" href="ressources/css/style.css">
+</head>
+<body>
+    <head>
+        <nav>
+            
+        </nav>
+    </head>
+
+    <div id="container">
+        du texte de fou
+        <div class="container-first-child">
+
+        </div>
+        <div class="row1">
+            <input type="text" name="name" id="">
+        </div>
+        <div class="row2">
+            <input type="email" name="email" id="">
+        </div>
+        <div class="row3">
+            <input type="password" name="password" id="">
+        </div>
+        <div class="row3">
+            <input type="password" name="password1" id="">
+        </div>
+        <input type="submit" value="Envoyer" name="send">
+    </div>
+
+</body>
+</html>
